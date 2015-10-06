9a8e4a32733267ae8e93e9e3df3507494d853281
Kirsten Korevaar <kirsten@eml.cc>
2015-10-06 22:53:32 +0200

    replace button with form. Every reload uses new text

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 from wiki_JSON import categories, is_dead
+import random
 
 app = Flask(__name__)
 
@@ -9,13 +10,21 @@ def index():
     url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Dick%20Cheney&continue=&prop=categories&format=json'
     r = requests.get(url)
     d = categories(r)
+    dead_text = ["Dick Cheney is Dead!", "Dick is still dead", "Dick Cheney is no longer alive",
+                 "Dick Cheney has seized to be", "Yes, Dick Cheney is dead",
+                 "Yes, Dick Cheney is Dead!", "Yes, Dick is still dead", "No, Dick Cheney is no longer alive",
+                 "Yes, Dick Cheney has seized to be", "Yes, Dick Cheney is dead"]
+    alive_text = ["Dick Cheney is not dead yet...", "Dick Cheney is still alive",
+                  "Dick Cheney is still breathing", "Dick Cheney is not dead",
+                  "No, Dick Cheney is not dead yet...", "Yes, Dick Cheney is still alive",
+                  "Yes, yes, Dick Cheney is still breathing", "No, Dick Cheney is not dead"]
     if is_dead(d):
         name = "dead"
-        result = "Dick Cheney is Dead!"
+        result= dead_text[random.randrange(len(dead_text))]
     else:
         name = "alive"
-        result = "Dick Cheney is not dead yet..."
-    return render_template('index.html', name=name, result=result, text=name)
+        result = alive_text[random.randrange(len(alive_text))]
+    return render_template('index.html', name=name, result=result )
 
 if __name__ == '__main__':
     app.run()
